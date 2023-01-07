1039f6162c149a2555607690fde91c47bc592658
Vladislav Kotsev <vladygk8@gmail.com>
2023-01-07 21:55:18 +0200

    add: fire emoji

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
+  
+document.querySelectorAll(".hot").forEach(x=>x.textContent+=String.fromCodePoint(0x1f525));
+
 });
